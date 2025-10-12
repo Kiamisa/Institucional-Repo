891f30c6d82157c08be2415563fec34698acd292
Silas Henrique <silas10henriique@gmail.com>
2025-10-12 19:25:02 -0300

    Refactor code structure for improved readability and maintainability

---
+++ backend/src/test/java/br/uema/ppg/IntelligentAssistent/IntelligentAssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uema.ppg.IntelligentAssistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentAssistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
